28e613f900e5f76a3bba9c2c00a076722ad7142a
Md. Refayatul Islam <refayatul7@gmail.com>
2024-11-15 18:33:01 +0600

    add gradient backgroud

--- lib/main.dart
+++ lib/main.dart
@@ -5,27 +5,51 @@ void main() {
     MaterialApp(
       theme: ThemeData.dark(useMaterial3: true),
       home: const Scaffold(
-        body: GetBirthdaImage(),
+        body: GradientContainer(
+            Color.fromARGB(255, 48, 2, 216), Color.fromARGB(255, 96, 31, 248)),
       ),
     ),
   );
 }
 
-class GetBirthdaImage extends StatelessWidget {
-  const GetBirthdaImage({super.key});
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            color1,
+            color2,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: GetBirthdayImage(),
+      ),
+    );
+  }
+}
+
+class GetBirthdayImage extends StatelessWidget {
+  const GetBirthdayImage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const Image(
-          image: AssetImage('assets/images/quiz-logo.png'),
-        ),
+        const Image(image: AssetImage('assets/images/quiz-logo.png')),
         const SizedBox(height: 20),
-        const Text('Learn flutter in Fun Way'),
         TextButton(
           onPressed: () {
-            //Later
+            // Add your onPressed functionality here
           },
           style: TextButton.styleFrom(
             foregroundColor: Colors.white,
@@ -33,7 +57,7 @@ class GetBirthdaImage extends StatelessWidget {
               fontSize: 20,
             ),
           ),
-          child: const Text('Start Quiz'),
+          child: const Text('Click'),
         ),
       ],
     );
